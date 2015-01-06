a2f6aa5138b60b1aaf68584ffbfa2cc031b830df
Sartaj <sartaj@sartaj.me>
2015-01-06 15:20:14 -0600

    can import html via web components and css

--- inject.js
+++ inject.js
@@ -1,86 +1,122 @@
-(function(inject) { "use strict";
+(function(inject) {
+    "use strict";
 
-  inject = function(file) {
-    
+    inject = function(file) {
 
-    function getFileType(file) {
+        var firstScript;
 
-      var extension;
+        function readFirstScript() {
+            if (!firstScript || !firstScript.parentNode) {
+                firstScript = document.getElementsByTagName('script')[0];
+            }
+        }
 
-      extension = file.split('.');
-      extension = extension[extension.length-1];
 
-      return extension;
+        function getFileType(file) {
+
+            var fileType;
+
+            fileType = file.split('.');
+            fileType = fileType[fileType.length - 1];
+
+            console.log("fileType", fileType);
+
+            return fileType;
+
+        }
+
+        function injectFile(file, fileType) {
+
+            console.log('injectFile', fileType);
+
+            var injectFile = {
+                'js': injectJs,
+                'css': injectCss,
+                'html': injectHtml
+            }
+
+            injectFile[fileType](file);
+
+        }
+
+        function injectJs(file) {
+
+
+            console.log("injectJs", file)
+
+            var script = document.createElement('script');
+            script.src = file;
+
+            // Inject script into to document
+            readFirstScript();
+            firstScript.parentNode.insertBefore(script, firstScript);
+
+        }
+
+
+        function injectCss(file) {
+
+            console.log("injectCss", file)
+
+            // Create stylesheet link
+            var link = document.createElement('link');
+
+            // Add attributes
+            link.href = file;
+            link.rel = 'stylesheet';
+
+            // Technique to force non-blocking loading from:
+            // https://github.com/filamentgroup/loadCSS/blob/master/loadCSS.js#L20
+            link.media = 'only x';
+            link.type = 'text/css';
+
+            // Inject script into to document
+            readFirstScript();
+            firstScript.parentNode.insertBefore(link, firstScript);
+
+        }
+
+
+        function injectHtml(file) {
+
+            console.log("injectHtml", file);
+
+            // Create stylesheet link
+            var link = document.createElement('link');
+
+            // Add attributes
+            link.href = file;
+            link.rel = 'html';
+
+            // Inject script into to document
+            readFirstScript();
+            // firstScript.parentNode.insertBefore(link, firstScript);
+
+        }
+
+        function run() {
+
+            var fileType = getFileType(file);
+            console.log("fileType", fileType);
+            injectFile(file, fileType);
+
+        }
+
+        run();
+
     }
 
-    function injectFile(extension) {
-
-      var injectFile = {
-        'js': injectJs(file),
-        'css': injectCss(file),
-        'html': injectHtml(file)
-      }
-
-      injectFile[extension];
-
+    if (typeof exports === 'object') {
+        // CommonJS support
+        module.exports = inject;
+    } else if (typeof define === 'function' && define.amd) {
+        // support AMD
+        define(function() {
+            return inject;
+        });
+    } else {
+        // support browser
+        window.inject = inject;
     }
-
-    function injectJs(file) {
-      console.log('inject', file)
-      var script = document.createElement('script');
-      script.src = file;
-
-    }
-
-
-    function injectCss(file) {
-
-    // Create stylesheet link
-      var link = document.createElement('link');
-
-      // Add attributes
-      link.href = file;
-      link.rel = 'stylesheet';
-
-      // Technique to force non-blocking loading from:
-      // https://github.com/filamentgroup/loadCSS/blob/master/loadCSS.js#L20
-      link.media = 'only x';
-      link.type = 'text/css';
-
-    }
-
-
-    function injectHtml(file) {
-
-      // var link = document.createElement('link');
-
-      // // Add attributes
-      // link.href = file;
-      // link.rel = 'imports';
-
-    }
-
-    function run() {
-
-      var fileType = getFileType(file);
-
-      injectFile(file, fileType);
-
-    }
-
-    run();
-
-  }
-
-if (typeof exports === 'object') {
-  // CommonJS support
-  module.exports = inject;
-} else if (typeof define === 'function' && define.amd) {
-  // support AMD
-  define(function() { return inject; });
-} else {
-  // support browser
-  window.inject = inject;
-}
 
 })({});
